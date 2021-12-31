e84fba11522312d77230556c8da10ddc4c9ba074
Alexander96p <96xanderp@gmail.com>
2021-12-30 20:31:50 -0500

    modificacion del main para ejecutar procesos fuera del contenedor

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 docker build -t base -f dockerfiles/base-dockerfile .
 docker build -t java -f dockerfiles/java-dockerfile .
 docker build -t java-repl -f dockerfiles/java-repl-dockerfile .
-# docker build -t main -f dockerfiles/main-dockerfile .
+docker build -t main -f dockerfiles/main-dockerfile .
